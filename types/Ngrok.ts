215f5970069d526b20fdeafbda7d057c827a7f8f
df1g45 <daffa1g45@gmail.com>
2023-08-17 20:10:10 +0700

    up

--- types/Ngrok.ts
+++ types/Ngrok.ts
@@ -1,10 +1,10 @@
 class Ngrok {
     static urls: { [key: string]: string } = {
-        httpx: "http://localhost:8000",
-        asynchttpx: "http://localhost:8000",
-        threadhttpx: "http://localhost:8000",
-        playwright: "http://localhost:8000",
-        playwrighthttpx: "http://localhost:8000",
+        httpx: "https://b797-34-126-65-220.ngrok-free.app",
+        asynchttpx: "https://b797-34-126-65-220.ngrok-free.app",
+        threadhttpx: "https://b797-34-126-65-220.ngrok-free.app",
+        playwright: "https://b797-34-126-65-220.ngrok-free.app",
+        playwrighthttpx: "https://b797-34-126-65-220.ngrok-free.app",
         selenium: "http://localhost:8000",
         seleniumhttpx: "http://localhost:8000",
       };
